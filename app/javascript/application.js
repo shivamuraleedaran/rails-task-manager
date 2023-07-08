53b2ffeaefe209f5981e99a735cc5df3c3616779
Shiva Muraleedaran <shivamuraleedaran@hotmail.com>
2023-07-08 10:43:06 +1000

    code added delete functionality added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+require("@rails/ujs").start();
+require("jquery");
+
+
 import "@hotwired/turbo-rails"
 import "controllers"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
+
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+
+  # root "tasks#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
